3cf37be574921173d9a4f92f0315cfd0651f0996
LTaylor137 <61397207+LTaylor137@users.noreply.github.com>
2020-09-10 11:24:31 +1000

    Copied Lachies Example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { ArticlesEnvelope, Article } from "./article";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'project';
+  title = 'angularconduit2020';
+
+  public art: Article[];
+  private httpClient: HttpClient;
+  constructor(httpClient: HttpClient) {
+    this.httpClient = httpClient;
+  }
+
+
+  login() {
+    let request = this.httpClient.get<ArticlesEnvelope>("https://swindev.me/articles");
+    request.subscribe((response) => {
+      console.log(response);
+      this.art = response.articles;
+    });
+  }
+
+
 }
